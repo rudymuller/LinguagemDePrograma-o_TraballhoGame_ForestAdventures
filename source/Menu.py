052b7790865597f8b00d2a11207cc89c515054ee
Rudy Gomes <rudygomes19@gmail.com>
2025-10-16 20:20:28 -0300

    Created asset folder Added assets for Menu and Level1 BG Menu: Background, sound and text creation

--- source/Menu.py
+++ source/Menu.py
@@ -1,9 +1,39 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame
+from pygame import Surface, Rect
+from pygame.font import Font
+
+from source.const import MENU_OPTION, WIN_WIDTH, C_ORANGE, C_WHITE
+
 
 class Menu:
     def __init__(self, window):
         self.window = window
+        self.surf = pygame.image.load('./asset/MenuBg.png')
+        self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
-        pass
+        pygame.mixer_music.load('./asset/Menu.mp3')
+        pygame.mixer_music.play(-1)
+        while True:
+            self.window.blit(source=self.surf, dest=self.rect)
+            self.menu_text(80, 'FOREST', C_ORANGE, ((WIN_WIDTH / 2), 80))
+            self.menu_text(80, 'ADVENTURES', C_ORANGE, ((WIN_WIDTH / 2), 130))
+
+            for i in range(len(MENU_OPTION)):
+                self.menu_text(25, MENU_OPTION[i], C_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
+
+            pygame.display.flip()
+
+            # Check for all events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()  # Close window
+                    quit()  # end pygame
+
+    def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(center=text_center_pos)
+        self.window.blit(source=text_surf, dest=text_rect)
--- source/const.py
+++ source/const.py
@@ -1,10 +1,15 @@
-
+# C
+C_ORANGE = (255, 128, 0)
+C_WHITE = (255, 255, 255)
+C_YELLOW = (255, 255, 128)
+C_GREEN = (0, 128, 0)
+C_CYAN = (0, 128, 128)
 
 
 # M
 MENU_OPTION = ('SINGLE PLAYER',
                '2 PLAYERS',
-               'SCORE',
+               'INSTRUCTIONS',
                'EXIT')
 
 
