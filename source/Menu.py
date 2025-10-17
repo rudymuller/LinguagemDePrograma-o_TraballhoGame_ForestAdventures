7e40445f3759e7e9bfbd7bb15f99ddaf1b4cd07d
Rudy Gomes <rudygomes19@gmail.com>
2025-10-16 23:29:15 -0300

    Optimizing Entity and Menu Improving Level1 asset: added Level1.mp3

--- source/Menu.py
+++ source/Menu.py
@@ -10,7 +10,7 @@ from source.const import MENU_OPTION, WIN_WIDTH, C_ORANGE, C_WHITE, C_YELLOW
 class Menu:
     def __init__(self, window):
         self.window = window
-        self.surf = pygame.image.load('./asset/MenuBg.png')
+        self.surf = pygame.image.load('./asset/MenuBg.png').convert_alpha()
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
